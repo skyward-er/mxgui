775f54ba66a1bb1a2de70f80076d1361b4d1cde0
EmilioCorigliano <emilio.corigliano@mail.polimi.it>
2022-06-08 17:32:53 +0200

    [death stack v3] modified library in order to work with death stack v3

--- mxgui/drivers/display_stm32f4discovery.cpp
+++ mxgui/drivers/display_stm32f4discovery.cpp
@@ -39,6 +39,7 @@ using namespace miosix;
     || defined(_BOARD_STM32F429ZI_SKYWARD_ROGALLINA) \
     || defined(_BOARD_STM32F429ZI_SKYWARD_DEATHST)   \
     || defined(_BOARD_STM32F429ZI_SKYWARD_DEATHST_X) \
+    || defined(_BOARD_STM32F429ZI_SKYWARD_DEATHST_V3) \
     || defined(_BOARD_STM32F429ZI_HRE_TEST_STAND) \
     || defined(_BOARD_STM32F429ZI_PARAFOIL)
 
